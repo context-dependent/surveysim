c26f018cd95173f186bec389f6596eca912c12c4
root <root@L96.BPA.LOCAL>
2023-06-01 14:29:49 -0400

    Basic structures in place

--- R/survey.R
+++ R/survey.R
@@ -2,15 +2,17 @@
 new_survey <- function(
     name = character(), 
     project = character(), 
+    response_rate = numeric(),
     respondent_id = character(), 
     questions = list()
 ) {
     stopifnot(is.character(name))
     stopifnot(is.character(project))
     stopifnot(is.character(respondent_id))
-    
+
     structure(
         questions,
+        response_rate = response_rate,
         respondent_id = respondent_id,  
         name = name, 
         project = project, 
